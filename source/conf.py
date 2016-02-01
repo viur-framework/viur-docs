ced511c089aa65de4e7efd29242aeb752a7032b1
Tim Jünemann <tim.juene@gmail.com>
2016-02-01 12:29:02 +0100

    New documentation template

--- source/conf.py
+++ source/conf.py
@@ -132,7 +132,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'classic'
+html_theme = 'mbtemplate'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -144,6 +144,7 @@ html_theme_options = {
 }
 """
 
+"""
 html_theme_options = {
     "sidebarbgcolor": "#CD1A1B",
     #"relbarbgcolor": "#CD1A1B",
@@ -151,9 +152,10 @@ html_theme_options = {
     #"footerbgcolor":  "#CD1A1B",
     "sidebarlinkcolor": "#CDCDCD"
 }
+"""
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ["."]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
